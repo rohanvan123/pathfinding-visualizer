963a64f7f3883a367adb9bd902bc02803748a88d
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-07-01 17:44:34 -0500

    Restarted next app

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import PathfindingVisualizer from "@/components /PathfindingVisualizer";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <PathfindingVisualizer />
-    </div>
-  );
+  return <main></main>;
 }
--- components /PathfindingVisualizer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PathfindingVisualizer = () => {
-  return <div></div>;
-};
-
-export default PathfindingVisualizer;
