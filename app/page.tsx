0f0417038693cda7a054cf5b8ddfac124b779ca0
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-07-01 17:10:00 -0500

    Create next app

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import PathfindingVisualizer from "@/components /PathfindingVisualizer";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <PathfindingVisualizer />
+    </div>
+  );
+}
---
+++ components /PathfindingVisualizer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PathfindingVisualizer = () => {
+  return <div></div>;
+};
+
+export default PathfindingVisualizer;
