ca6a860e0dfda90f5773196e3ec85c86acf46c87
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-07-01 19:23:03 -0500

    added color updated function

--- components/PathfindingVisualizer.tsx
+++ components/PathfindingVisualizer.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Node from "./Node";
 
 interface Node {
@@ -7,7 +9,7 @@ interface Node {
   color: string;
 }
 
-let grid: Node[][] = [];
+let initialGrid: Node[][] = [];
 
 for (let r = 0; r < 10; r++) {
   let row: Node[] = [];
@@ -19,11 +21,20 @@ for (let r = 0; r < 10; r++) {
     };
     row.push(n);
   }
-  grid.push(row);
+  initialGrid.push(row);
 }
 
 const PathfindingVisualizer = () => {
-  grid[5][5].color = "green-300";
+  const [grid, setGrid] = useState(initialGrid);
+
+  const updateNodeColor = (row: number, col: number, color: string) => {
+    setGrid((prevGrid) => {
+      const newGrid = [...prevGrid];
+      newGrid[row][col].color = color;
+      return newGrid;
+    });
+  };
+  console.log(grid);
   return (
     <div className="m-auto mt-[50px] w-[400px] h-[400px] border-green-300 border-[1px] grid grid-cols-10">
       {grid.map((row, rowIdx) => {
@@ -31,6 +42,9 @@ const PathfindingVisualizer = () => {
           <button
             key={`${rowIdx}-${colIdx}`}
             className={`h-[40px] w-[40px] border-black border-[1px] border-solid p-[0px] bg-${node.color}`}
+            onClick={() => {
+              updateNodeColor(rowIdx, colIdx, "green-300");
+            }}
           ></button>
         ));
       })}
