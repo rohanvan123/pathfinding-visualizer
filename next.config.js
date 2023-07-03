44c2700a329a536706f41735e51a4b26e64f0c32
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-07-03 18:42:00 -0500

    Test deployment to gh-pages 3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = {
-    basePath: '/pathfinding-visualizer',
-    assetPrefix: '/pathfinding-visualizer',
-  };
+const nextConfig = {
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+   
+  module.exports = nextConfig
